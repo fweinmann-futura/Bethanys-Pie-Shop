80a6a2d31b531fee21fc5e0f23a0ebaea3ec4f29
f.weinmann <f.weinmann@futura-solutions.de>
2022-10-10 15:12:38 +0200

    Employees created

---
+++ BethanysPieShopHRM/Employee.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM
+{
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+
+        public DateTime birthDay;
+
+        const int minimalHoursWorkedUnit = 1;
+
+        public Employee(string first, string last, string em, DateTime bd) : this(first, last, em, bd, 0)
+        {
+        }
+        public Employee(string first, string last, string em, DateTime bd, double rate)
+        {
+            firstName = first;
+            lastName = last;
+            email = em;
+            birthDay = bd;
+            hourlyRate = rate;
+        }
+
+        public void PerformWork()
+        {
+            //numberOfHoursWorked++;
+            PerformWork(minimalHoursWorkedUnit);
+            //Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)!");
+        }
+        public void PerformWork(int numberOfHours)
+        {
+            numberOfHoursWorked += numberOfHours;
+            Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHours} hour(s)!");
+        }
+        public int CalculateBonus(int bonus)
+        {
+            if (numberOfHoursWorked > 10)
+                bonus *= 2;
+            Console.WriteLine($"The employee got a bonus of {bonus}");
+            return bonus;
+        }
+        public int CalculateBonusAndBonusTax(int bonus, ref int bonusTax)
+        {
+            if (numberOfHoursWorked > 10)
+                bonus *= 2;
+            if (bonus >= 200)
+            {
+                bonusTax = bonus / 10;
+                bonus -= bonusTax;
+            }
+            Console.WriteLine($"The employee got a bonus of {bonus} and the tax on the bonus is {bonusTax}");
+            return bonus;
+        }
+        public double ReceiveWage(bool resetHours = true)
+        {
+            wage = numberOfHoursWorked * hourlyRate;
+            Console.WriteLine($"{firstName} {lastName} has received a {wage} for {numberOfHoursWorked} hour(s) of work.");
+
+            if (resetHours)
+                numberOfHoursWorked = 0;
+
+            return wage;
+         
+        }
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail:\t\t{email}\nBirthday: {birthDay.ToShortDateString()}\n");
+        }
+    }
+}
---
+++ BethanysPieShopHRM/Program.cs
@@ -0,0 +1,108 @@
+﻿
+
+using BethanysPieShopHRM;
+using System.Text;
+
+//int a = 42;
+//int aCopy = a;
+//Console.WriteLine($"Value of a: {a} and a value of copy of a: {aCopy}");
+//aCopy = 100;
+//Console.WriteLine($"Value of a: {a} and a value of copy of a: {aCopy}");
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
+
+Employee bethany = new Employee("Bethany", "Smith", "bethany@snowball.de", new DateTime(1979, 1, 16), 25);
+
+//bethany.PerformWork(25);
+
+string name = "bethany";
+string anotherName = name;
+name += " smith";
+
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Another name: " + anotherName);
+
+string upperCaseName = name.ToUpper();
+
+Console.WriteLine("Name: " + name);
+Console.WriteLine("Uppercase name: " + upperCaseName);
+
+string indexes = string.Empty;
+
+for (int i= 0; i <2500; i++)
+{
+    indexes += indexes.ToString();
+}
+
+string firstName = "Bethany";
+string lastName = "Smith";
+
+StringBuilder builder = new StringBuilder();
+
+builder.Append("Last name: ");
+builder.AppendLine(lastName);
+builder.Append("First name: ");
+builder.Append(firstName);
+string result = builder.ToString();
+
+StringBuilder builder2 = new StringBuilder();
+
+for (int i = 0; i < 2500; i++)
+{
+    builder2.Append(i);
+    builder2.Append(" ");
+}
+string list = builder2.ToString();
+
+//int minimumBonus = 100;
+//int receivedBonus = bethany.CalculateBonus(minimumBonus);
+//Console.WriteLine($"The minimum bonus is {minimumBonus} and {bethany.firstName} has received a bonus of {receivedBonus}");
+
+//int minimumBonus = 100;
+//int bonusTax = 0;
+//int receivedBonus = bethany.CalculateBonusAndBonusTax(minimumBonus, ref bonusTax);
+//Console.WriteLine($"The minimum bonus is {minimumBonus}, the bonus tax is {bonusTax} and {bethany.firstName} has received a bonus of {receivedBonus}");
+
+//Employee testEmployee = bethany;
+//testEmployee.firstName = "Gill";
+//testEmployee.DisplayEmployeeDetails();
+//bethany.DisplayEmployeeDetails();
+
+
+//bethany.DisplayEmployeeDetails();
+
+//bethany.PerformWork();
+//bethany.PerformWork();
+//bethany.PerformWork();
+//bethany.PerformWork(5);
+//bethany.PerformWork();
+//bethany.PerformWork();
+
+//bethany.firstName = "John";
+//bethany.hourlyRate = 10;
+
+//bethany.DisplayEmployeeDetails();
+//bethany.PerformWork();
+//bethany.PerformWork(10);
+//bethany.PerformWork();
+//bethany.PerformWork();
+//bethany.PerformWork();
+//bethany.PerformWork();
+
+//string fn = bethany.firstName;
+
+//double receivedWageBethany = bethany.ReceiveWage(true);
+//Console.WriteLine($"Wage paid (message from Program): {receivedWageBethany}");
+
+
+//Employee george = new("George", "Jones", "george@snowball.de", new DateTime(1948, 3, 28), 30);
+
+//george.DisplayEmployeeDetails(); ;
+//george.PerformWork();
+//george.PerformWork(3);
+//george.PerformWork();
+//george.PerformWork();
+//george.PerformWork(8);
+
+//var receivedWageGeorge = george.ReceiveWage(true);
